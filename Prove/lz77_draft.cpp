becdbb0345c1ae625c5a67e7308db6982a544b01
gianmarco112 <g.perini@arraysystem.it>
2021-03-04 13:40:53 +0100

    Compressione fatta

--- Prove/lz77_draft.cpp
+++ Prove/lz77_draft.cpp
@@ -9,7 +9,32 @@ using std::vector;
 using std::pair;
 using std::max;
 
-vector<pair<int,int>> LZ77(const string& s);
+vector<pair<int,int>> LZ77(const string& s){
+    vector<pair<int,int>> sol;
+    //Scorro la stringa, se non ho mai trovato il carattere aggiungo il carattere al vettore
+    int pos=0;
+    while(pos<s.length()){
+        //trovo la sottostringa più lunga che io abbia già trovato
+
+        pair<int,int> best_match_so_far;
+
+        for(int i=0;i<pos;i++){
+        //confronto la posizione corrente con quelle precedenti a partire da 0
+            int len = 0;
+            while (pos+len<s.length() && s[pos+len]==s[i+len])len++;
+            if(len>best_match_so_far.second) best_match_so_far={i,len};
+            
+        }
+        if(best_match_so_far.second==0) best_match_so_far.first=s[pos];
+        //incremento pos
+        pos+=std::max(1,best_match_so_far.second);
+        //pos+= best_match_so_far.second+(best_match_so_far.second==0);
+        sol.push_back(best_match_so_far);
+    }
+
+    return sol;
+
+};
 
 string decompress(const vector<pair<int,int>>& C);
 
