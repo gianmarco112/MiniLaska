af309f05271e3942ebf6e18337de74435dc02c1a
Gianmarco Perini <883492@stud.unive.it>
2021-03-09 11:40:44 +0100

    ok

---
+++ Prove/Class.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+class complex{
+    private:
+    double r;
+    double i;
+    public:
+    //Costruttore
+    complex():r{0.0},i{0.0}{}
+    //Costruttore che inizializza in base alla chiamata
+    complex(double real, double immagin):r{real},i{immagin}{}
+    //Costruttore che usa modulo e angolo
+    complex(double mod,double ang, bool polar){
+        r=mod*cos(ang);
+        i=mod*sin(ang);
+    }
+    complex somma(const complex& a) const{
+        complex res{a};
+        res.r+=r;//r appartiene ad a, il complex passato come parametro
+        res.i+=i;
+        return res;
+    }
+
+    void coniuga(){
+        i=-i;
+    }
+};
+
+int main(){
+
+    return 0;
+}
