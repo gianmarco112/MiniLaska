7de26c2aa8299e5484f0e9c00fd49a9af233a366
Gianmarco Perini <883492@stud.unive.it>
2021-03-11 12:17:41 +0100

    Calcok

--- Prove/Calc/version1.cpp
+++ Prove/Calc/version1.cpp
@@ -1,20 +1,56 @@
 #include <iostream>
-void error(std::string s){
-    std::cout<<s<<std::endl;
-    exit(1);
+
+struct myerror{
+    std::string msg;
+};
+
+
+void evaluate(){
+
+    double lval=0, rval=0;
+    char op = 0;
+
+    std::cout << "insert expression (we accept +-*/): " << std::endl;
+    std::cout << "end expression with =" << std::endl;
+
+    std::cin >> lval >> op;
+
+    if(!std::cin) throw myerror {"invalid operand"};
+    
+    while(op != '='){
+
+        std::cin >> rval;
+
+        if(!std::cin) throw myerror {"invalid operand"};
+
+        switch(op){
+            case '+': lval += rval; break;
+            case '-': lval -= rval; break;
+            case '/': lval /= rval; break;
+            case '*': lval *= rval; break;
+            default: throw myerror {"invalid operand"};
+        }
+
+        std::cin >> op;
+
+    }
+
+    std::cout << lval << std::endl;
+
 }
+
+
+
 int main(){
-    int lval=0, rval=0;
-    char op;
-    std::cout<< "inser expression (+ or -):"<<std::endl;
-    std::cin>>lval >> op >> rval;
-    if(!std::cin){
-        error("invalid operand");
+
+    try{
+
+        evaluate();
+        
+    }catch(myerror e){
+
+        std::cout << "error!" << e.msg << std::endl;
+    
     }
-    if(op=='+'){
-        lval+=rval;
-    }else if(op=='-'){
-        lval-=rval;
-    }
-    std::cout<< lval<<std::endl;
+
 }
