4566a93cebe5c1529aae00484a9349ecc56f4370
gianmarco112 <g.perini@arraysystem.it>
2021-03-30 16:42:48 +0200

    Merge branch 'master' of https://github.com/gianmarco112/MiniLaska

--- Laska/Game.c
+++ Laska/Game.c
@@ -1,13 +1,14 @@
 
-
+#include <stdio.h>
 #include "MiniLaska.h"
 #include <stdlib.h>
 /*gcc MiniLaska.c -c -oMiniLaska.o -ansi -pedantic*/
 /*gcc Game.c -oLaska -ansi -pedantic MiniLaska.o*/
 int main() {
-    pair_t sol;
-    field_t *field=(field_t*)malloc(sizeof (field_t));
-    start_game2(field);
+    pair_t mossacpu;
+    /* field_t *field=(field_t*)malloc(sizeof (field_t)); */
+    field_t field;
+    start_game2(&field);
     
     /* while(!field.partita.END_OF_PLAY){
         stampa_field(&field);
@@ -30,9 +31,17 @@ int main() {
         cpu_turn(field);
         
     }*/  
-    stampa_field(field);
-    sol=cpu_pedina(*field,10,BLACK);
-    stampa_field(field);
-    free_pedine(field);
+    while(!field.partita.END_OF_PLAY){
+    stampa_field(&field);
+    mossacpu =cpu_turn(field);
+    spostamento_pedine(&field,BLACK,mossacpu.index,mossacpu.indexb);
+    printf("Score %d Index %d Indexb %d \n",mossacpu.score,mossacpu.index,mossacpu.indexb);
+    stampa_field(&field);
+    movable(WHITE,&field);
+    if(field.partita.END_OF_PLAY)
+        break;
+    sel_pedina(WHITE,&field);
+    }
+    free_pedine(&field);
     return 0;
 }
